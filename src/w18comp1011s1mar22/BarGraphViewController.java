8ba71d2680858fbc43efd4fe2f65d6a919e760b5
JaretWright <hkkA223>
2018-03-22 15:20:05 -0400

    Initial upload

---
+++ src/w18comp1011s1mar22/BarGraphViewController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w18comp1011s1mar22;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author jwright
+ */
+public class BarGraphViewController implements Initializable
+{
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        // TODO
+    }    
+    
+}
---
+++ src/w18comp1011s1mar22/W18COMP1011S1Mar22.java
@@ -0,0 +1,18 @@
+package w18comp1011s1mar22;
+
+/**
+ *
+ * @author jwright
+ */
+public class W18COMP1011S1Mar22
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
